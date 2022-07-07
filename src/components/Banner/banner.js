2e50b91d7a1814a0d8c4000f5398979a6ff4eb81
Akinkuowo Oluwaseun <akinseun1990@gmail.com>
2022-07-07 05:59:11 +0100

    adding cinema banner

--- src/components/Banner/banner.js
+++ src/components/Banner/banner.js
@@ -2,24 +2,44 @@ import React, { useEffect, useState } from 'react'
 
 /* Import axios code from axios.js in ApiRequest Folder */
 import axios from "../../ApiRequest/axios"
-import request from '../../ApiRequest/requests';
+import requests from '../../ApiRequest/requests';
+
+import "./Banner.css"
 
 const Banner = () => {
     const [movie, setMovie] = useState([])
 
     useEffect(()=> {
         async function fetchBanner(){
-            // const request = await axios.get(fetchUrl)
+            const request = await axios.get(requests.fetchNetflixOriginals)
+            setMovie(request.data.results[Math.floor(Math.random() * request.data.results.length -1)])
             // setMovies(request.data.results)
-            // return request
-        } 
+            return request
+        }
         fetchBanner()
     }, [])
+    console.log(movie)
 
+    const truncate = (str, n)=> {
+        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+    }
     return (
    
-    <header>
-        banner
+    <header className='banner'
+        style={{
+            backgroundSize: "cover",
+            backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie?.backdrop_path})`,
+            backgroundPosition: "center top"
+        }}
+    >
+        <div className='banner_contents'>
+            <h1 className='banner_title'>{movie?.title || movie?.name || movie?.original_name}</h1>
+            <diV className="banner_buttons">
+                <button className='banner_button'>PLAY </button>
+                <button className='banner_button'>MY LIST </button>
+                <h1 className='banner_description'>{truncate(movie?.overview, 150)}</h1>
+            </diV>
+        </div>
     </header>
   )
 }
